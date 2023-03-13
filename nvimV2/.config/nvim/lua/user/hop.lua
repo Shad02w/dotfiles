feb49b41753d847032d7f3a13bf131a3ca17faa1
Alvis Tse <alvistse123@gmail.com>
2023-03-13 15:15:58 +0800

    feat(nvimV2): hop char 2

--- nvimV2/.config/nvim/lua/user/hop.lua
+++ nvimV2/.config/nvim/lua/user/hop.lua
@@ -1,15 +1,9 @@
-local function jump_to_word()
-    require('hop').hint_words()
-end
-
-local function jump_to_line()
-    vim.cmd 'HopLine'
-end
 return {
     'phaazon/hop.nvim',
     keys = {
-        { '<leader>j', jump_to_word, 'Jump to word' },
-        { '<leader>J', jump_to_line, 'Jump to line' },
+        { 'm', ':HopChar2<cr>', 'Hop Char 2' },
+        { '<leader>j', ':HopWord<cr>', 'Jump to word' },
+        { '<leader>J', ':HopLine<cr>', 'Jump to line' },
     },
     config = true,
 }
