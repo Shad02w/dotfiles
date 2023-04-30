2f1bf19c6356bea7bf583b986714e8b63bfd15ab
Alvis Tse <alvistse123@gmail.com>
2023-04-30 15:25:59 +0800

    refactor(nvimV2): add vim lib to lua_ls

--- nvimV2/.config/nvim/lua/plugins/lsp/settings/lua_ls.lua
+++ nvimV2/.config/nvim/lua/plugins/lsp/settings/lua_ls.lua
@@ -1,9 +1,30 @@
+local library = {}
+
+---@param path_like string
+local function addLibrary(path_like)
+    for _, p in pairs(vim.fn.expand(path_like, false, true)) do
+        p = vim.loop.fs_realpath(p)
+        table.insert(library, p)
+    end
+end
+
+addLibrary '$VIMRUNTIME'
+addLibrary '~/.config/nvim'
+addLibrary '~/.local/share/nvim/lazy/*'
+
+print(vim.inspect(library))
+
 return {
     settings = {
         Lua = {
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = 'LuaJIT',
+                path = {
+                    -- Setup your lua path
+                    'lua/?.lua',
+                    'lua/?/init.lua',
+                },
             },
             format = {
                 enable = false,
@@ -14,7 +35,7 @@ return {
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file('', true),
+                library = library,
             },
             -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
