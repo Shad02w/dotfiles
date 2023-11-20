aedc0e6ed66992290ea36e2cf8326be0c45c435f
Shad02w <alvistse123@gmail.com>
2023-11-20 00:14:49 -0500

    chore: update file type config to use vim.filetype api

--- nvim/.config/nvim/lua/core/filetype.lua
+++ nvim/.config/nvim/lua/core/filetype.lua
@@ -1,13 +1,16 @@
--- Set default file type
-local function set(pattern, filetype)
-    vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-        pattern = pattern,
-        callback = function()
-            vim.cmd('set filetype=' .. filetype)
-        end,
-    })
-end
+-- set default file type for file pattern
 
-set({ 'Podfile', '*.podspec' }, 'ruby')
-set({ '.swcrc', '.babelrc', 'package.json' }, 'jsonc')
-set({ '.plist', '.mobileconfig' }, 'xml')
+vim.filetype.add {
+    pattern = {
+        -- jsonc
+        ['package.json'] = 'jsonc',
+        ['.swcrc'] = 'jsonc',
+        ['.babelrc'] = 'jsonc',
+        -- xml
+        ['.plist'] = 'xml',
+        ['.mobileconfig'] = 'xml',
+        -- ruby
+        ['Podfile'] = 'ruby',
+        ['*.podspec'] = 'ruby',
+    },
+}
