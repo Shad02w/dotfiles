566cf8912136ac652eb36d57b0e6c72ef4baf58c
Shad02w <alvistse123@gmail.com>
2023-12-11 22:11:14 -0500

    refactor(nvim2): add luasnip and update cmp pmenu style

--- nvim2/.config/nvim2/lua/plugins/nvim-cmp.lua
+++ nvim2/.config/nvim2/lua/plugins/nvim-cmp.lua
@@ -28,17 +28,38 @@ local kind_icons = {
 
 return {
     'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    event = { 'InsertEnter', 'CmdLineEnter' },
     dependencies = {
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-path', -- path completion
         'hrsh7th/cmp-buffer', -- search completion
         'hrsh7th/cmp-cmdline', -- cmd completion
-        'saadparwaiz1/cmp_luasnip', -- snippet completion
+        { 'saadparwaiz1/cmp_luasnip', dependencies = { 'L3MON4D3/LuaSnip' } }, -- snippet completion
     },
     config = function()
         local cmp = require 'cmp'
+        local highlight = 'Normal:Normal,FloatBorder:None,CursorLine:Visual,Search:None'
         cmp.setup {
+            view = {
+                entries = 'custom',
+            },
+            window = {
+                completion = cmp.config.window.bordered { winhighlight = highlight },
+                documentation = cmp.config.window.bordered { winhighlight = highlight },
+            },
+            formatting = {
+                fields = { 'kind', 'abbr', 'menu' },
+                format = function(entry, vim_item)
+                    vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+                    vim_item.menu = ({
+                        nvim_lsp = '[LSP]',
+                        luasnip = '[Snippet]',
+                        path = '[Path]',
+                        buffer = '[Buffer]',
+                    })[entry.source.name]
+                    return vim_item
+                end,
+            },
             mapping = {
                 ['<cr>'] = cmp.mapping.confirm { select = true },
                 ['<c-space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
@@ -59,18 +80,24 @@ return {
             },
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+            }, {
                 { name = 'path' },
             }, {
                 { name = 'buffer' },
             }),
             snippet = {
                 expand = function(args)
-                    vim.notify(vim.inspect(args))
+                    require('luasnip').lsp_expand(args.body)
                 end,
             },
-            experimental = {
-                native_menu = false,
-            },
+            -- window = {
+            --     completion = cmp.config.window.bordered(),
+            --     documentation = cmp.config.window.bordered(),
+            -- },
+            -- experimental = {
+            --     native_menu = true,
+            -- },
         }
 
         cmp.setup.cmdline('/', {
