a570b8932ad1d4fbd946d17c8dc1dc9ede65470b
Shad02w <alvistse123@gmail.com>
2023-12-16 00:38:14 -0500

    feat(nvim2): add copilot

---
+++ nvim2/.config/nvim2/lua/plugins/ai.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        'zbirenbaum/copilot.lua',
+        event = 'InsertEnter',
+        cmd = "Copilot",
+        config = function()
+            require('copilot').setup {
+                suggestion = {
+                    auto_trigger = true,
+                    keymap = {
+                        accept = '<C-S-;>',
+                    },
+                },
+                panel = {
+                    auto_refresh = true,
+                },
+            }
+        end,
+    },
+}
