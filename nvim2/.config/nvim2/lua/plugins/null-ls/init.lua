0bcad4a7025d5219626ab43aebbaac079dfaca76
Shad02w <alvistse123@gmail.com>
2023-12-16 14:58:02 -0500

    feat(nvim2): add cspell to null-ls

--- nvim2/.config/nvim2/lua/plugins/null-ls/init.lua
+++ nvim2/.config/nvim2/lua/plugins/null-ls/init.lua
@@ -1,12 +1,14 @@
 return {
     'nvimtools/none-ls.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    event = { 'BufReadPre' },
+    event = { 'BufReadPost' },
     config = function()
         local null_ls = require 'null-ls'
+        local cspell = require('plugins.null-ls.cspell')
         null_ls.setup {
             sources = {
                 null_ls.builtins.formatting.stylua,
+                cspell.create_diagnostics_source()
             },
         }
     end,
