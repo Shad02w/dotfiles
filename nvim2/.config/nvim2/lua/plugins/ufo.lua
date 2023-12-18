13cace9e6d98d61240a91e6c731b63c520c0c9ce
Shad02w <alvistse123@gmail.com>
2023-12-18 02:33:47 -0500

    feat(nvim2): add ufo to support lsp folding

---
+++ nvim2/.config/nvim2/lua/plugins/ufo.lua
@@ -0,0 +1,38 @@
+return {
+    'kevinhwang91/nvim-ufo',
+    event = { 'CursorMoved', 'CursorMovedI' },
+    dependencies = {
+        'kevinhwang91/promise-async',
+        'luukvbaal/statuscol.nvim',
+    },
+    cmd = {
+        'UfoEnable',
+        'UfoDisable',
+        'UfoInspect',
+        'UfoAttach',
+        'UfoDetach',
+        'UfoEnableFold',
+        'UfoDisableFold',
+    },
+    init = function()
+        vim.o.foldlevel = 99
+        vim.o.foldlevelstart = 99
+        vim.o.foldcolumn = '1'
+        vim.o.foldenable = true
+        vim.opt.fillchars:append { fold = ' ', foldsep = ' ', foldopen = '', foldclose = '' }
+
+        -- to remove the fold level index, you need to custom build neovim,
+        -- ref: https://github.com/kevinhwang91/nvim-ufo/issues/4#issuecomment-1500423577
+    end,
+    config = function()
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+        ---@diagnostic disable-next-line: missing-fields
+        require('ufo').setup {
+            provider_selector = function()
+                return { 'lsp', 'indent' }
+            end,
+        }
+    end,
+}
