f4944da6dbcf58d683aabe2d5dc1e4b3647df01f
Shad02w <alvistse123@gmail.com>
2023-12-19 23:36:53 -0500

    refactor(nvim2): color on float panel

--- nvim2/.config/nvim2/lua/plugins/theme.lua
+++ nvim2/.config/nvim2/lua/plugins/theme.lua
@@ -15,6 +15,16 @@ return {
                 overrides = function(colors)
                     local theme = colors.theme
                     return {
+                        NormalFloat = { bg = 'none' },
+                        FloatBorder = { bg = 'none' },
+                        FloatTitle = { bg = 'none' },
+                        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+                        -- Popular plugins that open floats will link to NormalFloat by default;
+                        -- set their background accordingly if you wish to keep them dark and borderless
+                        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
                         -- cmp
                         -- Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
                         -- PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
