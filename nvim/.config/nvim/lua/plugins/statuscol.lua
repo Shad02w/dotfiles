508cbf44d1d8d338a61388a913064d13006e85db
Shad02w <alvistse123@gmail.com>
2024-01-11 03:29:27 -0500

    refactor(nvim): add vim-dadbod

--- nvim/.config/nvim/lua/plugins/statuscol.lua
+++ nvim/.config/nvim/lua/plugins/statuscol.lua
@@ -5,7 +5,7 @@ return {
         local builtin = require 'statuscol.builtin'
         require('statuscol').setup {
             relculright = true,
-            ft_ignore = { 'DiffviewFiles', 'notify', 'Trouble', 'neo-tree', 'netrw' },
+            ft_ignore = { 'DiffviewFiles', 'notify', 'Trouble', 'neo-tree', 'netrw', 'dbui' },
             segments = {
                 { text = { ' ' } },
                 {
