ad4305a60379c74410a5c01afe0802ba5db8adce
Shad02w <alvistse123@gmail.com>
2024-03-26 17:59:02 -0400

    chore(nvim): update <c-;> as copilot compelete key

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -8,7 +8,7 @@ return {
                 suggestion = {
                     auto_trigger = true,
                     keymap = {
-                        accept = '<C-S-;>',
+                        accept = '<C-;>',
                     },
                 },
                 panel = {
