6b88d5c0d382eabbd9eabcbc2381ae6ec6331a2e
Shad02w <alvistse123@gmail.com>
2024-04-28 00:54:24 -0400

    refactor(wezterm): update config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,22 +1,28 @@
 local wezterm = require 'wezterm'
 
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 local config = wezterm.config_builder()
 
-config.font_size = 16
+config.font_size = 14
 config.line_height = 1.3
 config.font = wezterm.font { family = 'JetBrainsMono Nerd Font', weight = 'Bold' }
+
+-- tab and window
+config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
-config.window_decorations = 'RESIZE'
-config.force_reverse_video_cursor = true
 config.show_new_tab_button_in_tab_bar = false
-config.macos_window_background_blur = 70
+config.window_decorations = 'RESIZE'
+
 config.window_padding = {
-    top = 10,
+    top = 30,
     left = 10,
     right = 10,
     bottom = 0,
 }
+
+config.force_reverse_video_cursor = true
 config.colors = {
     foreground = '#c5c9c5',
     background = '#181616',
