ce724f791b63899d24c3f32938d260bb7457abd2
Shad02w <alvistse123@gmail.com>
2024-11-01 20:17:39 -0400

    fix(nvim): load Commment.nvim when press gcc

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -3,8 +3,11 @@ return {
     keys = {
         { 'gc', mode = { 'n', 'v' }, desc = 'Comment toggle linewise' },
         { 'gb', mode = { 'n', 'v' }, desc = 'Comment toggle blockwise' },
+        { 'gcc', mode = { 'n', 'v' }, desc = 'Comment toggle line' },
+        { 'gbc', mode = { 'n', 'v' }, desc = 'Comment toggle block' },
     },
     dependencies = {
+        'JoosepAlviste/nvim-ts-context-commentstring',
         'nvim-treesitter/nvim-treesitter',
     },
     config = function()
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
     'nvim-treesitter/nvim-treesitter',
     event = { 'VeryLazy' },
     dependencies = {
-        { 'JoosepAlviste/nvim-ts-context-commentstring', commit = '6c30f3c8915d7b31c3decdfe6c7672432da1809d' },
         'nvim-treesitter/nvim-treesitter-textobjects',
         -- HACK: remove when https://github.com/windwp/nvim-ts-autotag/issues/125 closed.
         -- Bugs with self closing tags
