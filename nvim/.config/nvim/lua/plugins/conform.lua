45b5a98853f186224179b3d90e63862573d970b1
Shad02w <alvistse123@gmail.com>
2025-03-19 20:28:57 -0400

    feat(nvim): add astro formatter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -102,6 +102,7 @@ return {
                 javascript = jsFormatter,
                 typescriptreact = jsFormatter,
                 javascriptreact = jsFormatter,
+                astro = jsFormatter,
                 svelte = jsFormatter,
                 json = jsFormatter,
                 jsonc = jsFormatter,
