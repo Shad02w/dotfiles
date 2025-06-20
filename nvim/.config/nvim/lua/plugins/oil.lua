2edc65a666b15d2fb7782771cf99a2db8a6d0bdd
Shad02w <alvistse123@gmail.com>
2025-06-20 01:12:50 -0400

    refactor(nvim): oil.nvim show hidden files

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -14,6 +14,7 @@ return {
             lsp_file_methods = {
                 enabled = false,
             },
+            view_options = { show_hidden = true },
         }
 
         local parse_url = function(url)
